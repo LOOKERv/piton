a856c7848b2176aaa7ae9525af53086be739ed5c
looker n a <gera.nikitin1996@gmail.com>
2025-02-03 17:58:33 +0300

    2 часа занятий

--- 321.py
+++ 321.py
@@ -1,1 +1,15 @@
-print('hello world')
+
+
+def summ(a, b):
+   # a = input('ww a')
+  #  print('принято а')
+   # b = input('ww b')
+   # print('принято b')
+    sum = a + b
+    print('hello,', sum)
+    print('hello,')
+    print(sum)
+
+
+summ(4, 5)
+https: // youtu.be/cfJrtx-k96U?t = 7518
