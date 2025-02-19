589b4f88ea778cafe0443a062181dc179a9f5326
LOOKERv <gera.nikitin1996@mail.ru>
2025-02-19 11:21:31 +0300

    Update 123.py
    добавил коммент

--- 123.py
+++ 123.py
@@ -3,7 +3,7 @@ import os
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# Пример использования
+# Пример использования очистки экрана
 clear()
 a,b,c,d = int(input()), int(input()), int(input()), int(input())
 print((a+b+c+d)*3)
