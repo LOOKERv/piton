1bc826da773354775f19b153680f62bbdd467376
LOOKERv <gera.nikitin1996@mail.ru>
2025-02-24 17:30:43 +0300

    последняя задача

--- 123.py
+++ 123.py
@@ -7,28 +7,14 @@ def clear():
 
 # Пример использования
 clear()
-n = int(input())
-print((n//4)+1)
-print(n % 4)
-print(4-n)
-
-20 = 4 * 5
-19 5*4-1
-18 5*4-2
-17 5*4-3
-
-16 4*4
-15 4*4-2
-
-1 4*1-3
-2 4*1-2
-3 4*1-1
-4 4*1-0
-
-5 4*2-3
-6 4*2-2
-7 4*2-1
-8 4*2-0
-
-n 4*n//**-(4-n)
-https: // stepik.org/lesson/284816/step/18?auth = registration & unit = 266160
+a_1, b_1, a_2, b_2 = int(input()), int(input()), int(input()), int(input())
+if (a_1 == b_2) or (a_2 == b_1):#точка
+    if a_1 == b_2:
+        print(a_1)
+    else:
+        print(a_2)
+elif (a_1 == a_2) and (b_1 == b_2): #равны
+    print(a_1, b_1)
+elif (b_1<a_2) or (b_2>a_1) :
+    print('пустое множество') # не пересекаются
+#https://stepik.org/lesson/265082/step/11?thread=solutions&unit=246030
